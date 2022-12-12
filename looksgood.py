b9016246d0611528dcf1dbcd6a1f4b11f1ebefc7
Tarun1231805 <65174896+Tarun1231805@users.noreply.github.com>
2022-12-12 16:46:52 +0530

    Create looksgood.py

---
+++ looksgood.py
@@ -0,0 +1,55 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Creating ChatBot Instance
+chatbot = ChatBot(
+    'CoronaBot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter',
+        'chatterbot.logic.BestMatch',
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand. I am still learning.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
+
+training_data_quesans = open('training_data/online_store_QnA_1.txt').read().splitlines()
+training_data_personal = open('training_data/ques_ans.text').read().splitlines()
+
+training_data = training_data_quesans + training_data_personal
+
+trainer = ListTrainer(chatbot)
+trainer.train(training_data)
+
+# Training with English Corpus Data
+trainer_corpus = ChatterBotCorpusTrainer(chatbot)
+trainer_corpus.train(
+    'chatterbot.corpus.english'
+)
+
+# from chatterbot import ChatBot
+# from flask import Flask, render_template, request
+
+# app = Flask(__name__)
+# app.static_folder = 'static'
+
+# @app.route("/")
+# def home():
+    # return render_template("index.html")
+
+# @app.route("/get")
+# def get_bot_response():
+    # userText = request.args.get('msg')
+    # return str(chatbot.get_response(userText))
+
+# if __name__ == "__main__":
+    # app.run()
+
+# if __name__ == "__main__":
+    # app.run()
